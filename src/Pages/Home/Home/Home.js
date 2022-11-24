6f28e9db86d826aee93e81ea41c61ed231b7a915
dev-alamin1 <dev.alaminmondal@gmail.com>
2022-11-24 08:21:12 +0600

    newsletter added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Advertisements } from '../Advertisements/Advertisements';
 import { Banner } from '../Banner/Banner';
 import { Categories } from '../Categories/Categories';
+import { Newsletter } from '../Newsletter/Newsletter';
+
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <Advertisements/>
             <Categories/>
+            <Newsletter/>
+            
         </div>
     );
 };
