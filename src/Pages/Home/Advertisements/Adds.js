39c0aef439303a192bd34f43687c1c0d174733ad
dev-alamin1 <dev.alaminmondal@gmail.com>
2022-11-27 22:20:42 +0600

    advertise product show

---
+++ src/Pages/Home/Advertisements/Adds.js
@@ -0,0 +1,117 @@
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../Context/Authprovider";
+import { BiCheckCircle } from 'react-icons/bi';
+const Adds = ({
+  pro,
+  setProductFullInfo
+}) => {
+  // pro = product
+  const {
+    brand,
+    category_id,
+    description,
+    location,
+    originalPrice,
+    productImg,
+    productCondition,
+    productName,
+    resalePrice,
+    sellerName,
+    sellerEmail,
+    sellerMobile,
+    yearOfPurchase,
+  } = pro;
+
+     // check seller is verified or not 
+     const [sellerInfo,setSellerInfo] = useState();
+     const [sellerVerificationLoading,setSellerVerificationLoading] = useState(true);
+ 
+       useEffect(()=>{
+           fetch(`http://localhost:5000/checkSellerVerify?email=${sellerEmail}`)
+           .then(res=>res.json())
+           .then(data=>{
+ 
+            setSellerInfo(data)
+            setSellerVerificationLoading(false);
+           })
+ 
+       },[]);
+  
+
+  const {user,loading} = useContext(AuthContext);
+
+  
+
+  
+ if(loading)
+ {
+  return <div>user loading .. </div>
+ }
+
+  return (
+    <div className=" transition-shadow duration-300 bg-white rounded shadow-sm">
+      <img src={productImg} className=" w-full h-64" alt="" />
+      <div className="p-5 border border-t-0">
+
+        <p className="mb-2 text-gray-700">
+          <span className="text-red-400 font-bold">Name</span> : {productName}{" "}
+          <br /> <span className="text-red-400 font-bold">Brand</span> : {brand}
+        </p>
+
+        <p className="mb-2 text-gray-700">
+          <span className="text-red-400 font-bold">Original Price</span> :{" "}
+          {originalPrice}Tk <br />{" "}
+          <span className="text-orange-500 font-bold">Resale Price</span> :{" "}
+          {resalePrice}Tk
+        </p>
+
+        <p className="mb-2 text-gray-700">
+          <span className="text-red-400 font-bold">Used Time </span> :{" "}
+          {yearOfPurchase} years
+        </p>
+
+        <p className="mb-2 text-gray-700">
+          <span className="text-red-400 font-bold">Product Condition </span> :{" "}
+          {productCondition}
+        </p>
+
+        <p className="mb-2 text-gray-700">
+           <div className="flex items-center">
+           <span className="text-red-400 font-bold"> Seller Name </span> :
+          {` ${ sellerName?sellerName:'Not found'}`} <span>{sellerInfo?.verified === true && <BiCheckCircle className="text-blue-600 ml-1"/>}</span>
+            </div>    
+        </p>
+
+        <p className="mb-2 text-gray-700">
+          <span className="text-red-400 font-bold">Seller Mobile </span> :{" "}
+          {sellerMobile}
+        </p>
+
+        <p className="mb-2 text-gray-700">
+          <span className="text-red-400 font-bold">Seller Email </span> :{" "}
+          {sellerEmail}
+        </p>
+
+        <p className="mb-2 text-gray-700">
+          <span className="text-red-400 font-bold">Location</span> : {location}
+        </p>
+
+        <p className="mb-2 text-gray-700">
+          <span className="text-red-400 font-bold"> Description </span> :{" "}
+          {description}
+        </p>
+
+        <label
+          htmlFor="product-booked-modal"
+          onClick={() => setProductFullInfo(pro)}
+          className="bg-orange-400 px-2 py-1 rounded-sm hover:bg-orange-600 hover:text-white"
+        >
+          Book Now
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default Adds;
--- src/Pages/Home/Advertisements/Advertisements.js
+++ src/Pages/Home/Advertisements/Advertisements.js
@@ -1,166 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import Adds from "./Adds";
+
+
 export const Advertisements = () => {
-  return (
-    <div className="pt-10 pb-2 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl ">
-      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-        <div>
-          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-            Advertisement
-          </p>
-        </div>
-        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-          <span className="relative inline-block">
-            <svg
-              viewBox="0 0 52 24"
-              fill="currentColor"
-              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-            >
-              <defs>
-                <pattern
-                  id="db164e35-2a0e-4c0f-ab05-f14edc6d4d30"
-                  x="0"
-                  y="0"
-                  width=".135"
-                  height=".30"
-                >
-                  <circle cx="1" cy="1" r=".7" />
-                </pattern>
-              </defs>
-              <rect
-                fill="url(#db164e35-2a0e-4c0f-ab05-f14edc6d4d30)"
-                width="52"
-                height="24"
-              />
-            </svg>
-            <span className="relative">The</span>
-          </span>{" "}
-          quick, brown fox jumps over a lazy dog
-        </h2>
-        <p className="text-base text-gray-700 md:text-lg">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque rem aperiam, eaque ipsa quae.
-        </p>
-      </div>
-      <div className="grid  gap-5 mb-8 lg:grid-cols-3 sm:mx-auto w-full">
-        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
-          <img
-            src="https://images.pexels.com/photos/139829/pexels-photo-139829.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-            className="object-cover w-full h-64"
-            alt=""
-          />
-          <div className="p-5 border border-t-0">
-            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
-              <a
-                href="/"
-                className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
-                aria-label="Category"
-                title="traveling"
-              >
-                traveling
-              </a>
-              <span className="text-gray-600">— 28 Dec 2020</span>
-            </p>
-            <a
-              href="/"
-              aria-label="Category"
-              title="Film It!"
-              className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              Film It!
-            </a>
-            <p className="mb-2 text-gray-700">
-              Sed ut perspiciatis unde omnis iste natus error sit sed quia
-              consequuntur magni voluptatem doloremque.
-            </p>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-            >
-              Learn more
-            </a>
-          </div>
-        </div>
 
-        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
-          <img
-            src="https://images.pexels.com/photos/139829/pexels-photo-139829.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-            className="object-cover w-full h-64"
-            alt=""
-          />
-          <div className="p-5 border border-t-0">
-            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
-              <a
-                href="/"
-                className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
-                aria-label="Category"
-                title="traveling"
-              >
-                traveling
-              </a>
-              <span className="text-gray-600">— 28 Dec 2020</span>
-            </p>
-            <a
-              href="/"
-              aria-label="Category"
-              title="Film It!"
-              className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              Film It!
-            </a>
-            <p className="mb-2 text-gray-700">
-              Sed ut perspiciatis unde omnis iste natus error sit sed quia
-              consequuntur magni voluptatem doloremque.
-            </p>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-            >
-              Learn more
-            </a>
-          </div>
-        </div>
+ 
+  const {data:products = [],isLoading} = useQuery({
+    queryKey:['products'],
+    queryFn:async()=>{
+      const res = await fetch(`http://localhost:5000/advertised_products`);
+      const data = await res.json();
+      return data;
+    }
+  });
 
-        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
-          <img
-            src="https://images.pexels.com/photos/139829/pexels-photo-139829.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-            className="object-cover w-full h-64"
-            alt=""
-          />
-          <div className="p-5 border border-t-0">
-            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
-              <a
-                href="/"
-                className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
-                aria-label="Category"
-                title="traveling"
-              >
-                traveling
-              </a>
-              <span className="text-gray-600">— 28 Dec 2020</span>
-            </p>
-            <a
-              href="/"
-              aria-label="Category"
-              title="Film It!"
-              className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              Film It!
-            </a>
-            <p className="mb-2 text-gray-700">
-              Sed ut perspiciatis unde omnis iste natus error sit sed quia
-              consequuntur magni voluptatem doloremque.
-            </p>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-            >
-              Learn more
-            </a>
+
+    const [productFullInfo,setProductFullInfo] = useState({});
+
+    
+
+    return (
+        <div className="pt-10 pb-2 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl ">
+          <div className="grid  gap-5 mb-8 lg:grid-cols-3 sm:mx-auto w-full">
+           
+           {
+            products.map(pro=>
+            <Adds key={pro._id} pro={pro}  
+             setProductFullInfo ={setProductFullInfo} />)
+           }
+  
           </div>
+
+           
+
         </div>
-      </div>
-    </div>
-  );
+      );
+  
+  
 };
